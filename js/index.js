f90feb9db886aca54a114e971749d567ec53fae7
Tamara Rivera <tamita.rivera@gmail.com>
2017-05-02 23:29:17 -0700

    Added the mirror functionality for most of the inputs.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,22 @@
 /* globals $ */
-$('input').on('input change', function() {
-  
-  $(this).closest("tr").find('.value').text($(this).val());
-  $(this).closest("tr").find('input').val($(this).val());
-});
+$('input').on('input change', mirror);
+
+function mirror() {
+	var element = $(this);
+	var row = element.closest("tr");
+
+	if(element.is(':radio')) {
+		row.find('input').val([element.val()]);
+		row.find('.value').text(element.val());
+	}
+	else {
+		if(element.is(':checkbox')) {
+			row.find('input').prop('checked', element.prop('checked'));
+			row.find('.value').text(element.prop('checked'));
+		}
+		else {
+			row.find('input').val(element.val());
+			row.find('.value').text(element.val());
+		}
+	}
+}

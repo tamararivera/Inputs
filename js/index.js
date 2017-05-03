2e090f9667b1c44339d43d7932cf7e16431325bd
Tamara Rivera <tamita.rivera@gmail.com>
2017-05-02 23:38:38 -0700

    Added the mirror functionality for textarea and select inputs.

--- js/index.js
+++ js/index.js
@@ -20,3 +20,23 @@ function mirror() {
 		}
 	}
 }
+
+$('select').on('input change', selectFunction);
+
+function selectFunction() {
+	var element = $(this);
+	var row = element.closest("tr");
+
+	row.find('select').val(element.val());
+	row.find('.value').text(element.val());
+}
+
+$('textarea').on('input change', textareaFunction);
+
+function textareaFunction() {
+	var element = $(this);
+	var row = element.closest("tr");
+
+	row.find('textarea').val(element.val());
+	row.find('.value').text(element.val());
+}
